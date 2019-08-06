877a48ff7fb02d4ed6b0f78fdeba01f53b66073c
Anup Sethuram <anup.sethuram@safetyculture.io>
2019-08-06 14:12:46 +1000

    SBT setup

---
+++ src/main/scala/org/c1/HeWo.scala
@@ -0,0 +1,10 @@
+package org.c1
+
+object HeWo extends App {
+  println("Welcome to the scala-sbt-world!")
+
+  val years = Seq(1942, 1600, 1456, 1992, 2016, 2002)
+  println(s"Earliest year ${years.min}")
+
+
+}
